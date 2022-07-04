430d6b6c2738639861f4a38cb337e684676f59c4
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-07-04 13:33:11 +0530

    welcome message

---
+++ src/addressBook/addressBook.java
@@ -0,0 +1,13 @@
+package addressBook;
+
+public class addressBook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to address book ");
+
+	}
+
+}
